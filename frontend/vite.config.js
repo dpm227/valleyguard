0398fe92e0e3977acccb77c052e2151d785187bb
dpm227 <dpm227@lehigh.edu>
2025-11-12 09:53:18 -0500

    updated gitignore, updated base in config, updated gh-pages workflow

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/valleyguard/",
   plugins: [react(), tailwindcss()],
   resolve: {
     // make sure only ONE copy of these is used at runtime
